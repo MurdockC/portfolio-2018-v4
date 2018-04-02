4b9086ebf8fb30a791fbc1f68c6828f147350d97
Caleb <caleb@ramseymediaworks.com>
2018-04-02 16:55:08 -0500

    started project gallery

--- index.php
+++ index.php
@@ -8,17 +8,24 @@
 	<section class="flex bg-p h-100 jc--c w-f">
 		<div class="flex h-100 ai--c w-1200">
 			<div class="flex column h-100 jc--c">
-			<h1 class="header-txt">Caleb<br>Murdock</h1>
-			<p>Designer - Coder</p>
-		</div>
+				<h1 class="header-txt">Caleb<br>Murdock</h1>
+				<p>Designer - Coder</p>
+			</div>
 		</div>
 	</section>
 	<section class="gallery flex bg-d h-100 jc--c w-f">
-		<div class="flex h-100 ai--c w-1200">
-			<div class="flex column h-100 jc--c">
-			<h1 class="header-txt">Caleb<br>Murdock</h1>
-			<p>Designer - Coder</p>
-		</div>
+		<div class="flex column h-100 ai--c jc--c w-1200">
+				<h1 class="txt-w">Projects</h1>
+				<div class="gallery flex w-100">
+					<div class="gallery_item gallery_item_1"></div>
+					<div class="gallery_item gallery_item_2"></div>
+					<div class="gallery_item gallery_item_3"></div>
+					<div class="gallery_item gallery_item_4"></div>
+					<div class="gallery_item gallery_item_5"></div>
+					<div class="gallery_item gallery_item_6"></div>
+					<div class="gallery_item gallery_item_7"></div>
+					<div class="gallery_item gallery_item_8"></div>
+				</div>
 		</div>
 	</section>
 </section>
